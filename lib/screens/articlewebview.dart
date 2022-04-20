3825931c5b49f1eb847c4f8ae4804a0dea12dc44
Oteng-Derrick <42379936+derrique12@users.noreply.github.com>
2022-04-20 09:26:35 +0000

    article web view

---
+++ lib/screens/articlewebview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ArticleWebView extends StatefulWidget {
+  const ArticleWebView({Key? key}) : super(key: key);
+
+  @override
+  State<ArticleWebView> createState() => _ArticleWebViewState();
+}
+
+class _ArticleWebViewState extends State<ArticleWebView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
